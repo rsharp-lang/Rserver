1c6369600d24afae0eb8f20d97235186bd4604ff
xieguigang <xie.guigang@gcmodeller.org>
2021-06-16 01:08:15 +0800

    Update http.R

--- script/http.R
+++ script/http.R
@@ -6,3 +6,17 @@ imports "http" from "Rhttp";
 
 [@info "the http port for listen, 80 port number is used by default."]
 const httpPort as integer = ?"--listen" || 80;
+
+http::http_socket()
+|> headers(
+	"X-Powered-By" = "R# server"
+)
+|> httpGet(function(req, response) {
+
+})
+|> httpPost(function(req, response) {
+
+})
+|> http("PUT", [req, response] => writeLines("HTTP PUT test success!", con = response))
+|> listen(port = httpPort)
+;
