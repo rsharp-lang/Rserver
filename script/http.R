afcef5874f3f731ec77040d5edb9f71854a337b5
xieguigang <xie.guigang@gcmodeller.org>
2021-06-16 12:54:23 +0800

    re-format code in Rstudio

--- script/http.R
+++ script/http.R
@@ -8,52 +8,52 @@ imports "http" from "Rhttp";
 const httpPort as integer = ?"--listen" || 80;
 
 const router as function(url) {
-	`${dirname(@script)}/../web.R/${url$path}.R`;
+  `${dirname(@script)}/../web.R/${url$path}.R`;
 }
 
 const handleHttpGet as function(req, response) {
-	const R as string = router(getUrl(req));
+  const R as string = router(getUrl(req));
 
-	print("request from the browser client:");
-	str(getUrl(req));
+  print("request from the browser client:");
+  str(getUrl(req));
 
-	print("view the request data headers:");
-	str(getHeaders(req));
-	
-	print("this is the unparsed raw text of the http header message:");
-	print(getHttpRaw(req));
+  print("view the request data headers:");
+  str(getHeaders(req));
 
-	if (file.exists(R)) {
-		writeLines(source(R), con = response);
-	} else {
-		response 
-		|> httpError(404, `the required Rscript file is not found on filesystem location: '${ normalizePath(R) }'!`)
-		;
-	}	
+  print("this is the unparsed raw text of the http header message:");
+  print(getHttpRaw(req));
+
+  if (file.exists(R)) {
+    writeLines(source(R), con = response);
+  } else {
+    response
+    |> httpError(404, `the required Rscript file is not found on filesystem location: '${ normalizePath(R) }'!`)
+    ;
+  }
 }
 
 const handleHttpPost as function(req, response) {
-	const R as string = router(getUrl(req));
+  const R as string = router(getUrl(req));
 
-	str(getUrl(req));
-	str(getHeaders(req));
-	
-	print(getHttpRaw(req));
+  str(getUrl(req));
+  str(getHeaders(req));
 
-	if (file.exists(R)) {
-		writeLines(source(R), con = response);
-	} else {
-		response 
-		|> httpError(404, `the required Rscript file is not found on filesystem location: '${ normalizePath(R) }'!`)
-		;
-	}	
+  print(getHttpRaw(req));
+
+  if (file.exists(R)) {
+    writeLines(source(R), con = response);
+  } else {
+    response
+    |> httpError(404, `the required Rscript file is not found on filesystem location: '${ normalizePath(R) }'!`)
+    ;
+  }
 }
 
 cat("\n\n");
 
 http::http_socket()
 |> headers(
-	"X-Powered-By" = "R# server"
+  "X-Powered-By" = "R# server"
 )
 |> httpMethod("GET", handleHttpGet)
 |> httpMethod("POST", handleHttpPost)
