9c75894b8bcc393d9cbcac2dbac8ec21ed1974cd
xieguigang <xie.guigang@gcmodeller.org>
2022-10-05 13:14:44 +0800

    Update VBHtmlTemplate.vb

--- src/VBHtmlTemplate.vb
+++ src/VBHtmlTemplate.vb
@@ -1,6 +1,19 @@
-﻿Imports Microsoft.VisualBasic.Scripting.MetaData
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
+Imports SMRUCC.WebCloud.VBScript
 
 <Package("vbhtml")>
 Module VBHtmlTemplate
 
+    <ExportAPI("rendering")>
+    Public Function Rendering(file As String, wwwroot As String,
+                              <RListObjectArgument>
+                              Optional symbols As list = Nothing,
+                              Optional env As Environment = Nothing) As Object
+
+        Return vbhtml.ReadHTML(wwwroot, file)
+    End Function
 End Module
