bf66998d86086aa101f8c8eb3ab0e10bd68209aa
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-06-06 22:07:00 +0800

    build docs

---
+++ @export/index.d.ts
@@ -0,0 +1,14 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=Rserver
+
+declare namespace Rserver {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+   /**
+   */
+   function file_transfer(localfile: any): object;
+}
