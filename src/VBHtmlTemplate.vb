f88ae119d8c5ae9d8492f08c5ae70f2941cefa3b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-04-17 14:36:21 +0800

    rendering and compile

--- src/VBHtmlTemplate.vb
+++ src/VBHtmlTemplate.vb
@@ -5,6 +5,9 @@ Imports SMRUCC.Rsharp.Runtime.Internal.Object
 Imports SMRUCC.Rsharp.Runtime.Interop
 Imports SMRUCC.WebCloud.VBScript
 
+''' <summary>
+''' vbhtml template render/compiler
+''' </summary>
 <Package("vbhtml")>
 Module VBHtmlTemplate
 
@@ -14,6 +17,20 @@ Module VBHtmlTemplate
                               Optional symbols As list = Nothing,
                               Optional env As Environment = Nothing) As Object
 
+        Dim vars As Dictionary(Of String, Object) = symbols.AsGeneric(Of Object)(env, [default]:="")
+        Dim html As String = vbhtml.ReadHTML(wwwroot, file, vars)
+
+        Return html
+    End Function
+
+    ''' <summary>
+    ''' compile the template file as single html file for publish release
+    ''' </summary>
+    ''' <param name="file"></param>
+    ''' <param name="wwwroot"></param>
+    ''' <returns></returns>
+    <ExportAPI("compile")>
+    Public Function compile(file As String, wwwroot As String) As Object
         Return vbhtml.ReadHTML(wwwroot, file)
     End Function
 End Module
