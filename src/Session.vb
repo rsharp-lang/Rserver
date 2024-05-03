1080374a87ea6a477eec03aa31cab2c7add17676
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-03 10:49:30 +0800

    get string value from session

---
+++ src/Session.vb
@@ -0,0 +1,35 @@
+﻿Imports Flute.Http.Configurations
+Imports Flute.Http.Core.Message
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports Microsoft.VisualBasic.Serialization.JSON
+Imports SMRUCC.Rsharp.Runtime
+
+<Package("session")>
+Module Session
+
+    Dim ssid As String
+    Dim config As Configuration
+
+    <ExportAPI("load")>
+    Public Function load(Optional env As Environment = Nothing) As String
+        Dim cookie_str As String = env.globalEnvironment.options.getOption("cookies")
+        Dim config_str As String = env.globalEnvironment.options.getOption("config")
+        Dim cookies As Cookies = Cookies.ParseCookies(cookie_str)
+        Dim ssid As String = cookies.GetCookie("session_id")
+        Session.ssid = ssid
+        Session.config = config_str.LoadJSON(Of Configuration)
+        Return ssid
+    End Function
+
+    <ExportAPI("session_id")>
+    Public Function session_id() As String
+        Return ssid
+    End Function
+
+    <ExportAPI("get_string")>
+    Public Function get_string(key As String) As String
+        Return Flute.SessionManager.Open(key, config).OpenKeyString(key)
+    End Function
+
+End Module
