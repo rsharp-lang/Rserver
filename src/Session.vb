2f55ff20349804c1885b4e7da4ad9f1c4bae851f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-19 20:07:31 +0800

    fix for session data access

--- src/Session.vb
+++ src/Session.vb
@@ -3,6 +3,7 @@ Imports System.IO
 Imports System.Text
 Imports Flute.Http.Configurations
 Imports Flute.Http.Core.HttpStream
+Imports Flute.SessionManager
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Net.HTTP
@@ -10,11 +11,15 @@ Imports Microsoft.VisualBasic.Scripting.MetaData
 Imports Microsoft.VisualBasic.Serialization.JSON
 Imports SMRUCC.Rsharp.Runtime
 
+''' <summary>
+''' session data access
+''' </summary>
 <Package("session")>
 Module Session
 
     Dim ssid As String
     Dim config As Configuration
+    Dim file As SessionFile
 
     <ExportAPI("load")>
     Public Function load(Optional env As Environment = Nothing) As String
@@ -38,8 +43,11 @@ Module Session
         Dim config_str As String = reader.form("configs")
         Dim cookies As Dictionary(Of String, String) = cookie_str.LoadJSON(Of Dictionary(Of String, String))
         Dim ssid As String = cookies.TryGetValue("session_id")
+
         Session.ssid = ssid
         Session.config = config_str.LoadJSON(Of Configuration)
+        Session.file = Flute.SessionManager.Open(ssid, config)
+
         Return ssid
     End Function
 
@@ -50,17 +58,22 @@ Module Session
 
     <ExportAPI("get_string")>
     Public Function get_string(key As String) As String
-        Return Flute.SessionManager.Open(key, config).OpenKeyString(key)
+        Return file.OpenKeyString(key)
     End Function
 
     <ExportAPI("get_number")>
     Public Function get_number(key As String) As Double
-        Return Flute.SessionManager.Open(key, config).OpenKeyDouble(key)
+        Return file.OpenKeyDouble(key)
     End Function
 
     <ExportAPI("get_integer")>
     Public Function get_integer(key As String) As Integer
-        Return Flute.SessionManager.Open(key, config).OpenKeyInteger(key)
+        Return file.OpenKeyInteger(key)
+    End Function
+
+    <ExportAPI("set_string")>
+    Public Function set_string(key As String, value As String) As Object
+        Return file.SaveKey(key, value)
     End Function
 
 End Module
