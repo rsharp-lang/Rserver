372738ff0d46d1067a88ec6d75771581fc00b299
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-05-20 21:35:13 +0800

    Update http.R

--- R/http.R
+++ R/http.R
@@ -1,3 +1,31 @@
+require(JSON);
+
+#' push local file for download
+#' 
 const file_transfer = function(localfile) {
     pushDownload(localfile);
+}
+
+const http_success = function(data) {
+    let response = {
+        code: 0,
+        info: data
+    };
+
+    response
+    |> JSON::json_encode(row.names = TRUE)
+    |> writeLines(con = buffer("text", mime = "application/json"))
+    ;
+}
+
+const http_error = function(data, err = 500) {
+    let response = {
+        code: err,
+        info: data
+    };
+
+    response
+    |> JSON::json_encode(row.names = TRUE)
+    |> writeLines(con = buffer("text", mime = "application/json"))
+    ;
 }
