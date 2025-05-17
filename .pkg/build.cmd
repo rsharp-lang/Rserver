41a4d51f455112d28b2c274d64652d1dd426ef6c
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2025-05-17 17:16:33 +0800

    run package msbuild

--- .pkg/build.cmd
+++ .pkg/build.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 SET drive=%~d0
-SET R_HOME=%drive%/GCModeller\src\R-sharp\App\net6.0
+SET R_HOME=%drive%/GCModeller\src\R-sharp\App\net8.0
 
 %R_HOME%/Rscript.exe --build /src ../ /save ./Rserver.zip --skip-src-build 
 %R_HOME%/R#.exe --install.packages ./Rserver.zip
